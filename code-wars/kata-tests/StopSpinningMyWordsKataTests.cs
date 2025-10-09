ca0dce55346bb71a12da81755357577beaa04029
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-09 11:34:31 -0300

    stop spinning my words kata

---
+++ code-wars/kata-tests/StopSpinningMyWordsKataTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using kata_solutions.StopSpinningMyWords;
+
+namespace stop_gninnips_my_sdrow_tests;
+
+public class StopSpinningMyWordsKataTests
+{
+    [Theory]
+    [InlineData("Hey fellow warriors", "Hey wollef sroirraw")]
+    [InlineData("This is a test", "This is a test")]
+    [InlineData("This is another test", "This is rehtona test")]
+    public void On_Success_Should_Validate_StopSpinningMyWordsKata(string input, string expectedOutput)
+    {
+        StopSpinningMyWordsKata.SpinWords(input).Should().Be(expectedOutput);
+    }
+}
