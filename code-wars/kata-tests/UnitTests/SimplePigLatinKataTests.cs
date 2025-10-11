42ce0116a9babe7421832da891181ee99ba4b38f
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-10 22:16:09 -0300

    SimplePigLatinKata

---
+++ code-wars/kata-tests/UnitTests/SimplePigLatinKataTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using katas.SimplePigLatin;
+
+namespace kata.tests.UnitTests;
+
+public class SimplePigLatinKataTests
+{
+    [Theory]
+    [InlineData("Pig latin is cool", "igPay atinlay siay oolcay")]
+    [InlineData("This is my string", "hisTay siay ymay tringsay")]
+    [InlineData("Hello world !", "elloHay orldway !")]
+    public void On_Success_Should_Validate_SimplePigLatinKata(string input, string expectedOutput)
+    {
+        SimplePigLatinKata.PigIt(input).Should().Be(expectedOutput);
+    }
+}
---
+++ code-wars/katas/SimplePigLatin/SimplePigLatinKata.cs
@@ -0,0 +1,6 @@
+﻿namespace katas.SimplePigLatin;
+
+public class SimplePigLatinKata
+{
+    public static string PigIt(string str) => string.Join(" ", str.Split(' ').Select(word => word.Any(c => char.IsPunctuation(c)) ? word : $"{word[1..]}{word[0]}ay"));
+}
