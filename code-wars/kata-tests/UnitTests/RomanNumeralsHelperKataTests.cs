6d3df63d8d1d1df395246b454b3cb4e54f6b14bd
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-13 10:59:31 -0300

    RomanNumeralsHelperKata

---
+++ code-wars/kata-tests/UnitTests/RomanNumeralsHelperKataTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using katas.RomanNumeralsHelper;
+
+namespace kata.tests.UnitTests;
+
+public class RomanNumeralsHelperKataTests
+{
+    [Theory]
+    [InlineData(1, "I")]
+    [InlineData(2, "II")]
+    [InlineData(4, "IV")]
+    [InlineData(2464, "MMCDLXIV")]
+    public void On_Success_Should_Validate_RomanNumeralsHelperKata_ToRoman(int input, string expectedOutput)
+    {
+        var output = RomanNumeralsHelperKata.ToRoman(input);
+
+        output.Should().Be(expectedOutput);
+    }
+
+    [Theory]
+    [InlineData("I", 1)]
+    [InlineData("II", 2)]
+    [InlineData("IV", 4)]
+    [InlineData("MMCDLXIV", 2464)]
+    public void On_Success_Should_Validate_RomanNumeralsHelperKata_FromRoman(string input, int expectedOutput)
+    {
+        var output = RomanNumeralsHelperKata.FromRoman(input);
+
+        output.Should().Be(expectedOutput);
+    }
+}
---
+++ code-wars/katas/RomanNumeralsHelper/RomanNumeralsHelperKata.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace katas.RomanNumeralsHelper;
+
+public class RomanNumeralsHelperKata
+{
+    public static string ToRoman(int n)
+    {
+        var romanNumberBuilder = new StringBuilder();
+        var romanNumbers = SetupValueByRomanNumberDictionary();
+
+        while (n > 0)
+        {
+            var romanNumber = romanNumbers.First(romanNumber => n >= romanNumber.Value);
+
+            romanNumberBuilder.Append(romanNumber.Key);
+            n -= romanNumber.Value;
+        }
+
+        return romanNumberBuilder.ToString();
+    }
+
+    public static int FromRoman(string romanNumeral)
+    {
+        var sum = 0;
+        var romanNumbers = SetupValueByRomanNumberDictionary();
+
+        while (romanNumeral.Length > 0)
+        {
+            var match = romanNumbers.Keys.First(romanNumeral.StartsWith);
+
+            sum += romanNumbers[match];
+            romanNumeral = romanNumeral[match.Length..];
+        }
+
+        return sum;
+    }
+
+    private static Dictionary<string, int> SetupValueByRomanNumberDictionary() => new()
+    {
+        ["M"] = 1000,
+        ["CM"] = 900,
+        ["D"] = 500,
+        ["CD"] = 400,
+        ["C"] = 100,
+        ["XC"] = 90,
+        ["L"] = 50,
+        ["XL"] = 40,
+        ["X"] = 10,
+        ["IX"] = 9,
+        ["V"] = 5,
+        ["IV"] = 4,
+        ["I"] = 1
+    };
+}
