626128f4e86d4b777102ffb793f0490fded03772
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-14 16:19:42 -0300

    GreedIsGood

---
+++ code-wars/kata-tests/UnitTests/GreedIsGoodKataTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using katas.GreedIsGood;
+
+namespace kata.tests.UnitTests;
+
+public class GreedIsGoodKataTests
+{
+    [Theory]
+    [InlineData(new int[] { 2, 3, 4, 6, 2 }, 0)]
+    [InlineData(new int[] { 4, 4, 4, 3, 3 }, 400)]
+    [InlineData(new int[] { 2, 4, 4, 5, 4 }, 450)]
+    [InlineData(new int[] { 1, 1, 2, 3, 6 }, 200)]
+    [InlineData(new int[] { 5, 5, 2, 3, 6 }, 100)]
+    public void On_Success_Should_Validate_GreedIsGoodKata(int[] input, int expectedOutput)
+    {
+        GreedIsGoodKata.Score(input).Should().Be(expectedOutput);
+    }
+}
---
+++ code-wars/katas/GreedIsGood/GreedIsGoodKata.cs
@@ -0,0 +1,22 @@
+﻿namespace katas.GreedIsGood;
+
+public class GreedIsGoodKata
+{
+    public static int Score(int[] dice) => dice.CountBy(value => value).Sum(GetScore);
+
+    private static int GetScore(KeyValuePair<int, int> pair) =>
+        pair switch
+        {
+            { Key: 1, Value: 3 } => 1000,
+            { Key: 6, Value: 3 } => 600,
+            { Key: 5, Value: 3 } => 500,
+            { Key: 4, Value: 3 } => 400,
+            { Key: 3, Value: 3 } => 300,
+            { Key: 2, Value: 3 } => 200,
+            { Key: 1, Value: 1 } => 100,
+            { Key: 5, Value: 1 } => 50,
+            { Key: 1 } => pair.Value * 100,
+            { Key: 5 } => pair.Value * 50,
+            _ => 0
+        };
+}
