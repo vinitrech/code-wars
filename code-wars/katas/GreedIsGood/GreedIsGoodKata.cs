aceda37854fbcd92e3858e558d7407b700d592fa
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-14 16:49:44 -0300

    optimizing calculations

--- code-wars/katas/GreedIsGood/GreedIsGoodKata.cs
+++ code-wars/katas/GreedIsGood/GreedIsGoodKata.cs
@@ -7,12 +7,12 @@ public class GreedIsGoodKata
     private static int GetScore(KeyValuePair<int, int> pair) =>
         pair switch
         {
-            { Key: 1 } => pair.Value >= 3 ? 1000 * (pair.Value / 3) + (pair.Value % 3 * 100) : pair.Value * 100,
-            { Key: 2 } => pair.Value >= 3 ? 200 * (pair.Value / 3) : 0,
-            { Key: 3 } => pair.Value >= 3 ? 300 * (pair.Value / 3) : 0,
-            { Key: 4 } => pair.Value >= 3 ? 400 * (pair.Value / 3) : 0,
-            { Key: 5 } => pair.Value >= 3 ? 500 * (pair.Value / 3) + (pair.Value % 3 * 50) : pair.Value * 50,
-            { Key: 6 } => pair.Value >= 3 ? 600 * (pair.Value / 3) : 0,
+            { Key: 1 } => 1000 * (pair.Value / 3) + (pair.Value % 3 * 100),
+            { Key: 2 } => 200 * (pair.Value / 3),
+            { Key: 3 } => 300 * (pair.Value / 3),
+            { Key: 4 } => 400 * (pair.Value / 3),
+            { Key: 5 } => 500 * (pair.Value / 3) + (pair.Value % 3 * 50),
+            { Key: 6 } => 600 * (pair.Value / 3),
             _ => 0
         };
 }
