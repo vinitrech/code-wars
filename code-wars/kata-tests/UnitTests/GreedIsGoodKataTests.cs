b2d2aa28ad7683ca7e6799398ace1ce595a69aa7
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-14 16:43:29 -0300

    GreedIsGood fixed

--- code-wars/kata-tests/UnitTests/GreedIsGoodKataTests.cs
+++ code-wars/kata-tests/UnitTests/GreedIsGoodKataTests.cs
@@ -11,6 +11,13 @@ public class GreedIsGoodKataTests
     [InlineData(new int[] { 2, 4, 4, 5, 4 }, 450)]
     [InlineData(new int[] { 1, 1, 2, 3, 6 }, 200)]
     [InlineData(new int[] { 5, 5, 2, 3, 6 }, 100)]
+    [InlineData(new int[] { 1, 1, 1, 1, 2 }, 1100)]
+    [InlineData(new int[] { 2, 2, 2, 2, 3 }, 200)]
+    [InlineData(new int[] { 3, 3, 3, 3, 4 }, 300)]
+    [InlineData(new int[] { 4, 4, 4, 4, 6 }, 400)]
+    [InlineData(new int[] { 5, 5, 5, 5, 6 }, 550)]
+    [InlineData(new int[] { 6, 6, 6, 6, 2 }, 600)]
+    [InlineData(new int[] { 1, 1, 1, 1, 1 }, 1200)]
     public void On_Success_Should_Validate_GreedIsGoodKata(int[] input, int expectedOutput)
     {
         GreedIsGoodKata.Score(input).Should().Be(expectedOutput);
--- code-wars/katas/GreedIsGood/GreedIsGoodKata.cs
+++ code-wars/katas/GreedIsGood/GreedIsGoodKata.cs
@@ -7,16 +7,12 @@ public class GreedIsGoodKata
     private static int GetScore(KeyValuePair<int, int> pair) =>
         pair switch
         {
-            { Key: 1, Value: 3 } => 1000,
-            { Key: 6, Value: 3 } => 600,
-            { Key: 5, Value: 3 } => 500,
-            { Key: 4, Value: 3 } => 400,
-            { Key: 3, Value: 3 } => 300,
-            { Key: 2, Value: 3 } => 200,
-            { Key: 1, Value: 1 } => 100,
-            { Key: 5, Value: 1 } => 50,
-            { Key: 1 } => pair.Value * 100,
-            { Key: 5 } => pair.Value * 50,
+            { Key: 1 } => pair.Value >= 3 ? 1000 * (pair.Value / 3) + (pair.Value % 3 * 100) : pair.Value * 100,
+            { Key: 2 } => pair.Value >= 3 ? 200 * (pair.Value / 3) : 0,
+            { Key: 3 } => pair.Value >= 3 ? 300 * (pair.Value / 3) : 0,
+            { Key: 4 } => pair.Value >= 3 ? 400 * (pair.Value / 3) : 0,
+            { Key: 5 } => pair.Value >= 3 ? 500 * (pair.Value / 3) + (pair.Value % 3 * 50) : pair.Value * 50,
+            { Key: 6 } => pair.Value >= 3 ? 600 * (pair.Value / 3) : 0,
             _ => 0
         };
 }
