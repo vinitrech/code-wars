cd22566260c2234076dbdcb6e8370257d761ce3d
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-15 14:22:51 -0300

    CanYouGetTheLoop Kata

---
+++ code-wars/kata-tests/UnitTests/CanYouGetTheLoopKataTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using katas.CanYouGetTheLoop;
+
+namespace kata.tests.UnitTests;
+
+public class CanYouGetTheLoopKataTests
+{
+    [Theory]
+    [InlineData(3, 30)]
+    [InlineData(3904, 1087)]
+    [InlineData(4, 3)]
+    public void On_Success_Should_Validate_CanYouGetTheLoopKata(int tailSize, int loopSize)
+    {
+        // arrange
+        var startNode = Node.CreateChain(tailSize, loopSize);
+
+        // act
+        var result = CanYouGetTheLoopKata.GetLoopSize(startNode);
+
+        // assert
+        result.Should().Be(loopSize);
+    }
+}
---
+++ code-wars/katas/CanYouGetTheLoop/CanYouGetTheLoopKata.cs
@@ -0,0 +1,66 @@
+﻿namespace katas.CanYouGetTheLoop;
+
+public class CanYouGetTheLoopKata
+{
+    public static int GetLoopSize(Node startNode)
+    {
+        var currentNode = startNode;
+        var nodeDictionary = new Dictionary<Node, int>();
+        var index = 0;
+
+        while (true)
+        {
+            if (!nodeDictionary.ContainsKey(currentNode!.GetNext()!))
+            {
+                nodeDictionary.Add(currentNode, index++);
+                currentNode = currentNode.GetNext();
+            }
+            else
+                return nodeDictionary.Count - nodeDictionary[currentNode.GetNext()!] + 1;
+        }
+    }
+}
+
+public class Node
+{
+    private Node? Next;
+
+    public Node? GetNext()
+    {
+        return Next;
+    }
+
+    private void SetNext(Node next)
+    {
+        Next = next;
+    }
+
+    public static Node CreateChain(int tailSize, int loopSize)
+    {
+        var prevNode = new Node();
+        var start = prevNode;
+
+        start.SetNext(start);
+
+        if (loopSize == 1)
+            return start;
+
+        for (int i = 1; i <= tailSize; ++i)
+        {
+            prevNode?.SetNext(new Node());
+            prevNode = prevNode?.GetNext();
+        }
+
+        var endLoop = prevNode;
+
+        for (int i = 1; i < loopSize; ++i)
+        {
+            prevNode?.SetNext(new Node());
+            prevNode = prevNode?.GetNext();
+        }
+
+        prevNode?.SetNext(endLoop!);
+
+        return start;
+    }
+}
