5f43e2b5525cc86c38b3d3413c0ef8ec76a85bcc
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-16 11:47:47 -0300

    MeanSquareErrorKata

---
+++ code-wars/kata-tests/UnitTests/MeanSquareErrorKataTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using katas.MeanSquareError;
+
+namespace kata.tests.UnitTests;
+
+public class MeanSquareErrorKataTests
+{
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }, 9d)]
+    [InlineData(new int[] { 10, 20, 10, 2 }, new int[] { 10, 25, 5, -2 }, 16.5d)]
+    [InlineData(new int[] { 0, -1 }, new int[] { -1, 0 }, 1d)]
+    public void On_Success_Should_Validate_MeanSquareErrorKata(int[] firstArray, int[] secondArray, double expectedOutput)
+    {
+        MeanSquareErrorKata.Solution(firstArray, secondArray).Should().Be(expectedOutput);
+    }
+}
---
+++ code-wars/katas/MeanSquareError/MeanSquareErrorKata.cs
@@ -0,0 +1,6 @@
+﻿namespace katas.MeanSquareError;
+
+public class MeanSquareErrorKata
+{
+    public static double Solution(int[] firstArray, int[] secondArray) => firstArray.Zip(secondArray, (first, second) => Math.Pow(Math.Abs(first - second), 2)).Average();
+}
