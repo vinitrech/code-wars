41c8c45175245527023a31bc6d321595b19ef279
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-16 15:51:43 -0300

    MovingZerosToTheEnd

---
+++ code-wars/kata-tests/UnitTests/MovingZerosToTheEndKataTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using katas.MovingZerosToTheEnd;
+
+namespace kata.tests.UnitTests;
+
+public class MovingZerosToTheEndKataTests
+{
+    [Theory]
+    [InlineData(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 }, new int[] { 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 })]
+    public void On_Success_Should_Validate_MovingZerosToTheEndKata(int[] sourceArray, int[] expectedArray)
+    {
+        var outputArray = MovingZerosToTheEndKata.MoveZeroes(sourceArray);
+
+        outputArray.Should().BeEquivalentTo(expectedArray);
+    }
+}
---
+++ code-wars/katas/MovingZerosToTheEnd/MovingZerosToTheEndKata.cs
@@ -0,0 +1,6 @@
+﻿namespace katas.MovingZerosToTheEnd;
+
+public class MovingZerosToTheEndKata
+{
+    public static int[] MoveZeroes(int[] arr) => [.. arr.GroupBy(x => x == 0).OrderBy(x => x.Key is true ? 0 : 1).SelectMany(g => g)];
+}
