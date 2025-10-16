a81a86afac50eb517602122bd3eace7751831ecf
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-16 16:58:19 -0300

    JosephusSurvivorKata

---
+++ code-wars/kata-tests/UnitTests/JosephusSurvivorKataTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using katas.JosephusSurvivor;
+
+namespace kata.tests.UnitTests;
+
+public class JosephusSurvivorKataTests
+{
+    [Theory]
+    [InlineData(7, 3, 4)]
+    [InlineData(11, 19, 10)]
+    [InlineData(40, 3, 28)]
+    [InlineData(14, 2, 13)]
+    [InlineData(100, 1, 100)]
+    [InlineData(1, 300, 1)]
+    [InlineData(2, 300, 1)]
+    [InlineData(5, 300, 1)]
+    [InlineData(7, 300, 7)]
+    [InlineData(300, 300, 265)]
+    public void On_Success_Should_Validate_JosephusSurvivorKata(int group, int step, int expectedOutput)
+    {
+        var output = JosephusSurvivorKata.JosSurvivor(group, step);
+
+        output.Should().Be(expectedOutput);
+    }
+}
---
+++ code-wars/katas/JosephusSurvivor/JosephusSurvivorKata.cs
@@ -0,0 +1,18 @@
+﻿namespace katas.JosephusSurvivor;
+
+public class JosephusSurvivorKata
+{
+    public static int JosSurvivor(int n, int k)
+    {
+        var group = Enumerable.Range(1, n).ToList();
+        var index = 0;
+
+        while(group.Count > 1)
+        {
+            index = (index + k - 1) % group.Count;
+            group.RemoveAt(index);
+        }
+
+        return group[0];
+    }
+}
