24dc5cd0888c02fc3ed37f00347d5d6efce1c841
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-16 17:22:41 -0300

    fixing indentation

--- code-wars/katas/JosephusSurvivor/JosephusSurvivorKata.cs
+++ code-wars/katas/JosephusSurvivor/JosephusSurvivorKata.cs
@@ -7,7 +7,7 @@ public class JosephusSurvivorKata
         var group = Enumerable.Range(1, n).ToList();
         var index = 0;
 
-        while(group.Count > 1)
+        while (group.Count > 1)
         {
             index = (index + k - 1) % group.Count;
             group.RemoveAt(index);
