07488b849e491aeccd4bd5952588303bdda31b08
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-17 17:15:19 -0300

    NextBiggerNumberWithTheSameDigitsKata

---
+++ code-wars/kata-tests/UnitTests/NextBiggerNumberWithTheSameDigitsKataTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using katas.NextBiggerNumberWithTheSameDigits;
+
+namespace kata.tests.UnitTests;
+
+public class NextBiggerNumberWithTheSameDigitsKataTests
+{
+    [Theory]
+    [InlineData(12, 21)]
+    [InlineData(513, 531)]
+    [InlineData(2017, 2071)]
+    [InlineData(414, 441)]
+    [InlineData(144, 414)]
+    [InlineData(531, -1)]
+    [InlineData(111, -1)]
+    [InlineData(9, -1)]
+    [InlineData(1234567890, 1234567908)]
+    [InlineData(1318113062, 1318113206)]
+    public void On_Success_Should_Validate_NextBiggerNumberWithTheSameDigitsKata(long input, long expectedOutput)
+    {
+        NextBiggerNumberWithTheSameDigitsKata.NextBiggerNumber(input).Should().Be(expectedOutput);
+    }
+}
---
+++ code-wars/katas/NextBiggerNumberWithTheSameDigits/NextBiggerNumberWithTheSameDigitsKata.cs
@@ -0,0 +1,28 @@
+﻿namespace katas.NextBiggerNumberWithTheSameDigits;
+
+public class NextBiggerNumberWithTheSameDigitsKata
+{
+    public static long NextBiggerNumber(long n)
+    {
+        var digits = n.ToString().ToCharArray();
+
+        for (var i = digits.Length - 1; i > 0; i--)
+            if (digits[i] > digits[i - 1])
+            {
+                var pivot = i - 1;
+                var suffix = digits[i..];
+                var minGreaterAfterPivot = (Value: suffix[0], Index: 0);
+
+                for (var suffixIndex = 0; suffixIndex < suffix.Length; suffixIndex++)
+                    minGreaterAfterPivot = suffix[suffixIndex] > digits[pivot] && suffix[suffixIndex] < minGreaterAfterPivot.Value ? (suffix[suffixIndex], suffixIndex) : minGreaterAfterPivot;
+
+                (digits[pivot], digits[minGreaterAfterPivot.Index + i]) = (digits[minGreaterAfterPivot.Index + i], digits[pivot]);
+
+                digits.AsSpan()[i..].Sort();
+
+                return long.Parse(new string(digits));
+            }
+
+        return -1;
+    }
+}
