67ae8bb4631b6b68661fecae69fcbd3c838295b6
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-18 10:18:19 -0300

    Scramblies

---
+++ code-wars/kata-tests/UnitTests/ScrambliesKataTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using katas.Scramblies;
+
+namespace kata.tests.UnitTests;
+
+public class ScrambliesKataTests
+{
+    [Theory]
+    [InlineData("rkqodlw", "world", true)]
+    [InlineData("cedewaraaossoqqyt", "codewars", true)]
+    [InlineData("katas", "steak", false)]
+    public void On_Success_Should_Validate_ScrambliesKata(string input, string wordToFind, bool expectedOutput)
+    {
+        ScrambliesKata.Scramble(input, wordToFind).Should().Be(expectedOutput);
+    }
+}
---
+++ code-wars/katas/Scramblies/ScrambliesKata.cs
@@ -0,0 +1,6 @@
+﻿namespace katas.Scramblies;
+
+public class ScrambliesKata
+{
+    public static bool Scramble(string str1, string str2) => str2.CountBy(character => character).All(characterCount => str1.Count(characterCount.Key) >= characterCount.Value);
+}
