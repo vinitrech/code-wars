2f0dbad66745a22aa832fd2654a7f7deb0c18c4d
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-19 20:08:30 -0300

    PermutationsKata

---
+++ code-wars/kata-tests/UnitTests/PermutationsKataKataTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using katas.Permutations;
+
+namespace kata.tests.UnitTests;
+
+public class PermutationsKataKataTests
+{
+    [Theory]
+    [InlineData("a", new string[] { "a" })]
+    [InlineData("ab", new string[] { "ab", "ba" })]
+    [InlineData("aabb", new string[] { "aabb", "abab", "abba", "baab", "baba", "bbaa" })]
+    public void On_Success_Should_Validate_PermutationsKata(string inputText, string[] expectedOutput)
+    {
+        PermutationsKata.SinglePermutations(inputText).Should().BeEquivalentTo(expectedOutput);
+    }
+}
---
+++ code-wars/katas/Permutations/PermutationsKata.cs
@@ -0,0 +1,7 @@
+﻿namespace katas.Permutations;
+
+public class PermutationsKata
+{
+    public static List<string> SinglePermutations(string s) =>
+        [.. s.Aggregate(new List<string> { "" }, (acc, c) => [.. acc.SelectMany(a => Enumerable.Range(0, a.Length + 1).Select(i => a.Insert(i, c.ToString())))]).Distinct()];
+}
