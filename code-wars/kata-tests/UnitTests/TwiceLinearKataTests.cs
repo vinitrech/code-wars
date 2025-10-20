96412aac97b334eecee9d08ed647df63933ccd87
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-20 14:34:34 -0300

    TwiceLinear

---
+++ code-wars/kata-tests/UnitTests/TwiceLinearKataTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using katas.TwiceLinear;
+
+namespace kata.tests.UnitTests;
+
+public class TwiceLinearKataTests
+{
+    [Theory]
+    [InlineData(10, 22)]
+    [InlineData(20, 57)]
+    [InlineData(30, 91)]
+    [InlineData(50, 175)]
+    [InlineData(100, 447)]
+    [InlineData(500, 3355)]
+    [InlineData(1000, 8488)]
+    [InlineData(2000, 19773)]
+    [InlineData(6000, 80914)]
+    [InlineData(60000, 1511311)]
+    public void On_Success_Should_Validate_TwiceLinearKata(int inputNumber, int expectedOutput)
+    {
+        TwiceLinearKata.DblLinear(inputNumber).Should().Be(expectedOutput);
+    }
+}
---
+++ code-wars/katas/TwiceLinear/TwiceLinearKata.cs
@@ -0,0 +1,22 @@
+﻿namespace katas.TwiceLinear;
+
+public class TwiceLinearKata
+{
+    public static int DblLinear(int n)
+    {
+        var sortedSet = new SortedSet<int>() { 1 };
+        var elements = new List<int>();
+
+        while (elements.Count <= n)
+        {
+            var current = sortedSet.Min;
+            sortedSet.Remove(current);
+            elements.Add(current);
+
+            sortedSet.Add(2 * current + 1);
+            sortedSet.Add(3 * current + 1);
+        }
+
+        return elements[n];
+    }
+}
