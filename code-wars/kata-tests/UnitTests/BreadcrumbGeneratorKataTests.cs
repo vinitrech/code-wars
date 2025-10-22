d1dff60c89e8546bc62595a901dd8fb9aef26a13
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-22 15:28:35 -0300

    BreadcrumbGenerator

---
+++ code-wars/kata-tests/UnitTests/BreadcrumbGeneratorKataTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using katas.BreadcrumbGenerator;
+
+namespace kata.tests.UnitTests;
+
+public class BreadcrumbGeneratorKataTests
+{
+    [Theory]
+    [InlineData("mysite.com/pictures/holidays.html", " : ", "<a href=\"/\">HOME</a> : <a href=\"/pictures/\">PICTURES</a> : <span class=\"active\">HOLIDAYS</span>")]
+    [InlineData("www.codewars.com/users/GiacomoSorbi?ref=CodeWars", " / ", "<a href=\"/\">HOME</a> / <a href=\"/users/\">USERS</a> / <span class=\"active\">GIACOMOSORBI</span>")]
+    [InlineData("www.microsoft.com/docs/index.htm#top", " * ", "<a href=\"/\">HOME</a> * <span class=\"active\">DOCS</span>")]
+    [InlineData("mysite.com/very-long-url-to-make-a-silly-yet-meaningful-example/example.asp", " > ", "<a href=\"/\">HOME</a> > <a href=\"/very-long-url-to-make-a-silly-yet-meaningful-example/\">VLUMSYME</a> > <span class=\"active\">EXAMPLE</span>")]
+    [InlineData("https://www.linkedin.com/in/giacomosorbi", " * ", "<a href=\"/\">HOME</a> * <a href=\"/in/\">IN</a> * <span class=\"active\">GIACOMOSORBI</span>")]
+    [InlineData("www.agcpartners.co.uk", " # ", "<span class=\"active\">HOME</span>")]
+    [InlineData("https://www.agcpartners.co.uk/index.html", " >>> ", "<span class=\"active\">HOME</span>")]
+    [InlineData("http://www.agcpartners.co.uk", " % ", "<span class=\"active\">HOME</span>")]
+    [InlineData("www.agcpartners.co.uk/", " * ", "<span class=\"active\">HOME</span>")]
+    [InlineData("www.very-long-site_name-to-make-a-silly-yet-meaningful-example.com/users/giacomo-sorbi", " + ", "<a href=\"/\">HOME</a> + <a href=\"/users/\">USERS</a> + <span class=\"active\">GIACOMO SORBI</span>")]
+    public void On_Success_Should_Validate_BreadcrumbGeneratorKata(string url, string separator, string expectedBreadcrumb)
+    {
+        BreadcrumbGeneratorKata.GenerateBC(url, separator).Should().Be(expectedBreadcrumb);
+    }
+}
---
+++ code-wars/katas/BreadcrumbGenerator/BreadcrumbGeneratorKata.cs
@@ -0,0 +1,24 @@
+﻿namespace katas.BreadcrumbGenerator;
+
+public class BreadcrumbGeneratorKata
+{
+    public static string GenerateBC(string url, string separator)
+    {
+        var wordsToIgnore = new List<string> { "the", "of", "in", "from", "by", "with", "and", "or", "for", "to", "at", "a" };
+        var urlParts = new Uri(new UriBuilder(url).ToString()).Segments.Skip(1).Where(segment => !segment.Contains("index."));
+        var sanitizedParts = urlParts.Select(s => s.Split('.')[0].TrimEnd('/').ToUpper()).Prepend("HOME");
+        var mountedParts = sanitizedParts.Select((s, i) => new
+        {
+            href = string.Concat(urlParts.Take(i)),
+            text = s.Length > 30
+                ? string.Concat(s.Split('-').Where(w => !wordsToIgnore.Contains(w, StringComparer.InvariantCultureIgnoreCase)).Select(c => c[0]))
+                : s.Replace("-", " ")
+        });
+
+        return string.Join(separator, mountedParts
+            .Select((x, i) =>
+                i < urlParts.Count()
+                    ? $"<a href=\"/{x.href}\">{x.text}</a>"
+                    : $"<span class=\"active\">{x.text}</span>"));
+    }
+}
