163073ce0be19c1a1cde36fb767ab053774c95e6
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-22 09:49:49 -0300

    Hamming numbers

---
+++ code-wars/kata-tests/UnitTests/HammingNumberKataTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using katas.HammingNumber;
+
+namespace kata.tests.UnitTests;
+
+public class HammingNumberKataTests
+{
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(2, 2)]
+    [InlineData(3, 3)]
+    [InlineData(4, 4)]
+    [InlineData(5, 5)]
+    [InlineData(6, 6)]
+    [InlineData(7, 8)]
+    [InlineData(8, 9)]
+    [InlineData(9, 10)]
+    [InlineData(10, 12)]
+    [InlineData(11, 15)]
+    [InlineData(12, 16)]
+    [InlineData(13, 18)]
+    [InlineData(14, 20)]
+    [InlineData(15, 24)]
+    [InlineData(16, 25)]
+    [InlineData(17, 27)]
+    [InlineData(18, 30)]
+    [InlineData(19, 32)]
+    public void On_Success_Should_Validate_HammingNumberKata(int input, long expectedOutput)
+    {
+        HammingNumberKata.Hamming(input).Should().Be(expectedOutput);
+    }
+}
---
+++ code-wars/katas/HammingNumber/HammingNumberKata.cs
@@ -0,0 +1,27 @@
+﻿namespace katas.HammingNumber;
+
+public class HammingNumberKata
+{
+    public static long Hamming(int n)
+    {
+        var sequence = new List<long>() { 1 };
+        var (i2, i3, i5) = (0, 0, 0);
+
+        while (sequence.Count < n)
+        {
+            var (next2, next3, next5) = (sequence[i2] * 2, sequence[i3] * 3, sequence[i5] * 5);
+            var nextValue = new List<long> { next2, next3, next5 }.Min();
+
+            sequence.Add(nextValue);
+
+            if (nextValue == next2)
+                i2 += 1;
+            if (nextValue == next3)
+                i3 += 1;
+            if (nextValue == next5)
+                i5 += 1;
+        }
+
+        return sequence[n - 1];
+    }
+}
