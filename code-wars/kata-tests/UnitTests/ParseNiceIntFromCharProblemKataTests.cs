021ab46b63051fda473c28e02c72baea722b13ce
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-23 14:16:31 -0300

    ParseNiceIntFromCharProblemKata

---
+++ code-wars/kata-tests/UnitTests/ParseNiceIntFromCharProblemKataTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using katas.ParseNiceIntFromCharProblem;
+
+namespace kata.tests.UnitTests;
+
+public class ParseNiceIntFromCharProblemKataTests
+{
+    [Theory]
+    [InlineData("4 years old", 4)]
+    [InlineData("5 years old", 5)]
+    [InlineData("6 years old", 6)]
+    public void On_Success_Should_Validate_ParseNiceIntFromCharProblemKata(string input, int expectedOutput)
+    {
+        ParseNiceIntFromCharProblemKata.GetAge(input).Should().Be(expectedOutput);
+    }
+}
---
+++ code-wars/katas/ParseNiceIntFromCharProblem/ParseNiceIntFromCharProblemKata.cs
@@ -0,0 +1,6 @@
+﻿namespace katas.ParseNiceIntFromCharProblem;
+
+public class ParseNiceIntFromCharProblemKata
+{
+    public static int GetAge(string inputString) => int.Parse(inputString[0].ToString());
+}
