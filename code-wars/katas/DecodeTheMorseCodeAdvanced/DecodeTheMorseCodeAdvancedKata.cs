50c627c36d4e810e1d4d4e8a3a901e55398e9f07
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-23 15:57:55 -0300

    DecodeTheMorseCodeAdvanced PARTIAL

---
+++ code-wars/katas/DecodeTheMorseCodeAdvanced/DecodeTheMorseCodeAdvancedKata.cs
@@ -0,0 +1,69 @@
+﻿namespace katas.DecodeTheMorseCodeAdvanced;
+
+//INCOMPLETE - NOT WORKING PROPERLY
+public class DecodeTheMorseCodeAdvancedKata
+{
+    public static string DecodeBits(string bits) =>
+        string.Join("   ", bits.Split("00000000")
+                .Select(part =>
+                {
+                    var translatedCharacters = part.Split("0000")
+                        .Where(part => !string.IsNullOrWhiteSpace(part))
+                        .Select(word =>
+                        {
+                            return string.Join("", word.Split("00")
+                                .Where(character => !string.IsNullOrWhiteSpace(character))
+                                .Select(character => character switch
+                                {
+                                    "11" => ".",
+                                    "111111" => "-",
+                                    _ => ""
+                                }));
+                        });
+
+                    return string.Join(" ", translatedCharacters);
+                })
+            );
+
+    public static string DecodeMorse(string morseCode)
+    {
+        var morseDictionary = SetupMorseDictionary();
+        var encodedWords = morseCode.Split("   ");
+        var decodedWords = encodedWords.Select(encodedWord => DecodeWord(morseDictionary, encodedWord));
+
+        return string.Join(" ", decodedWords).Trim();
+    }
+
+    private static string DecodeWord(Dictionary<string, string> morseDictionary, string encodedWord) =>
+        string.Join("", DecodeSymbols(morseDictionary, encodedWord));
+
+    private static IEnumerable<string> DecodeSymbols(Dictionary<string, string> morseDictionary, string encodedWord) =>
+        encodedWord.Split(" ").Where(symbol => !string.IsNullOrWhiteSpace(symbol)).Select(symbol => morseDictionary[symbol]);
+
+    private static Dictionary<string, string> SetupMorseDictionary() => new()
+    {
+        // Letters
+        { ".-", "A" },    { "-...", "B" },  { "-.-.", "C" },  { "-..", "D" },
+        { ".", "E" },     { "..-.", "F" },  { "--.", "G" },   { "....", "H" },
+        { "..", "I" },    { ".---", "J" },  { "-.-", "K" },   { ".-..", "L" },
+        { "--", "M" },    { "-.", "N" },    { "---", "O" },   { ".--.", "P" },
+        { "--.-", "Q" },  { ".-.", "R" },   { "...", "S" },   { "-", "T" },
+        { "..-", "U" },   { "...-", "V" },  { ".--", "W" },   { "-..-", "X" },
+        { "-.--", "Y" },  { "--..", "Z" },
+
+        // Numbers
+        { ".----", "1" }, { "..---", "2" }, { "...--", "3" }, { "....-", "4" },
+        { ".....", "5" }, { "-....", "6" }, { "--...", "7" }, { "---..", "8" },
+        { "----.", "9" }, { "-----", "0" },
+
+        // Punctuation and prosigns
+        { ".-.-.-", "." }, { "--..--", "," }, { "..--..", "?" }, { ".----.", "'" },
+        { "-.-.--", "!" }, { "-..-.", "/" },  { "-.--.", "(" },  { "-.--.-", ")" },
+        { ".-...", "&" },  { "---...", ":" }, { "-.-.-.", ";" }, { "-...-", "=" },
+        { ".-.-.", "+" },  { "-....-", "-" }, { "..--.-", "_" }, { ".-..-.", "\"" },
+        { "...-..-", "$" },{ ".--.-.", "@" },
+
+        // Special prosigns
+        { "...---...", "SOS" }
+    };
+}
