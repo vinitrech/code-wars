833970129906c8e61aa44116d1a7afbb04806345
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-23 13:09:55 -0300

    CountOnesInASegment

---
+++ code-wars/kata-tests/UnitTests/CountOnesInASegmentKataTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using katas.CountOnesInASegment;
+
+namespace kata.tests.UnitTests;
+
+public class CountOnesInASegmentKataTests
+{
+    [Theory]
+    [InlineData(5, 7, 7)]
+    [InlineData(12, 29, 51)]
+    public void On_Success_Should_Validate_CountOnesInASegmentKata(long left, long right, long expectedOutput)
+    {
+        CountOnesInASegmentKata.CountOnes(left, right).Should().Be(expectedOutput);
+    }
+}
---
+++ code-wars/katas/CountOnesInASegment/CountOnesInASegmentKata.cs
@@ -0,0 +1,36 @@
+﻿using System.Numerics;
+
+namespace katas.CountOnesInASegment;
+
+public class CountOnesInASegmentKata
+{
+    public static BigInteger CountOnes(long left, long right)
+    {
+        if (left > right) return 0;
+
+        return CountOnesUpTo(right) - CountOnesUpTo(left - 1);
+    }
+
+    private static long CountOnesUpTo(long n)
+    {
+        var total = 0L;
+
+        while (n > 0)
+        {
+            var p = 1L;
+            var k = 0;
+
+            while ((p << 1) <= n)
+            {
+                p <<= 1;
+                k++;
+            }
+
+            total += k * (p >> 1);
+            total += n - p + 1;
+            n -= p;
+        }
+
+        return total;
+    }
+}
