32421a7801937ff832e289bc71fb0c684373fa58
Vinicius Tonello Rech <vinitrech@gmail.com>
2025-10-24 09:27:43 -0300

    AddingBigNumbersKata

---
+++ code-wars/katas/AddingBigNumbers/AddingBigNumbersKata.cs
@@ -0,0 +1,8 @@
+﻿using System.Numerics;
+
+namespace katas.AddingBigNumbers;
+
+public class AddingBigNumbersKata
+{
+    public static string Add(string a, string b) => $"{BigInteger.Parse(a) + BigInteger.Parse(b)}";
+}
